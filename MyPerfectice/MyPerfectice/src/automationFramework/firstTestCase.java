25a718c9627b7d6ec1053a12658e3459948eff93
singhya <singhya@US1178303W4.ey.net>
2018-05-22 16:57:44 -0400

    This is working

---
+++ MyPerfectice/MyPerfectice/src/automationFramework/firstTestCase.java
@@ -0,0 +1,12 @@
+//http://store.demoqa.com/
+
+package automationFramework;
+
+public class firstTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//
+	}
+
+}
